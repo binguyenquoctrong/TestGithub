32e519370c98508987f0c3d6b2ea3a0dbbb3c5b8
Quoc <Quoc@TrongNQSE62667>
2017-10-28 11:12:57 +0700

    Testing for Github

---
+++ test git/src/test/git/TestGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.git;
+
+/**
+ *
+ * @author Quoc
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("holy shit");
+    }
+    
+}
